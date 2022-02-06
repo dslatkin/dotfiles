a4284d70fd58ad3bf297461fa2f9a2c978acbe1b
Daniel Slatkin <danielslatkin@gmail.com>
2022-02-06 03:38:26 +0000

    Set fish to be default shell

--- install.sh
+++ install.sh
@@ -8,5 +8,9 @@ curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debi
 sudo apt update
 sudo apt install fish -y
 
+# Change default shell to fish
+echo /usr/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/bin/fish
+
 # remove script
 rm "$0"

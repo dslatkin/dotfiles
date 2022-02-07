147ce455af96e49b550ef16edc9051b6c9f0095d
Daniel Slatkin <danielslatkin@gmail.com>
2022-02-07 01:27:37 +0000

    Improve fish install time

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "🚀 Dotfiles: Start install from $0"
 echo "🚀 Dotfiles: Install fish"
 echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_9.0/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
 curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_9.0/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
-sudo apt-get update
+sudo apt-get update -o Dir::Etc::SourceList='/etc/apt/sources.list.d/shells:fish:release:3.list'
 sudo apt-get install fish -y
 
 # Install starship binary binary

f1d883a40dafaf2047c246a3b8fedc0897a5ba9e
Daniel Slatkin <danielslatkin@gmail.com>
2022-02-07 01:57:08 +0000

    Refactor install noise

--- install.sh
+++ install.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
-echo "🚀 Dotfiles: Start install from $0"
+info() {
+  echo "🚀 Dotfiles: $*"
+}
 
-# Install fish
-echo "🚀 Dotfiles: Install fish"
+info Starting install "$0"
+
+info Install fish
 echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_9.0/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
 curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_9.0/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
 sudo apt-get update
 sudo apt-get install fish -y
 fish -v
 
-# Install starship binary binary
-echo "🚀 Dotfiles: Install starship binary"
+info Install starship binary
 bash -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes --verbose
 
-# Add starship to bash
-echo "🚀 Dotfiles: Add starship to bash"
+info Add starship to bash
 starship init bash >> ~/.bashrc
 
-# Change default shell to fish
-echo "🚀 Dotfiles: Change default shell to fish"
-# echo /usr/bin/fish | sudo tee -a /etc/shells
+info Change default shell to fish
 sudo chsh -s /usr/bin/fish
 
-# Add starship to fish
-echo "🚀 Dotfiles: Create fish config file"
-fish -c ''
-echo "🚀 Dotfiles: Add starship to fish"
+info Add starship to fish
+fish -c 'echo Creating fish config file'
 starship init fish >> ~/.config/fish/config.fish
 
-# Remove script
-rm "$0"
+info Finished install

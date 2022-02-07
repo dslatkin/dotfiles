d6a198426783b09f72829ce6369f5edfd1547b70
Daniel Slatkin <danielslatkin@gmail.com>
2022-02-07 00:43:43 +0000

    Add debug statements

--- install.sh
+++ install.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-echo "🚀 Installing dslatkin/dotfiles from $0"
+echo "🚀 Dotfiles: Start install from $0"
 
 # Install fish
+echo "🚀 Dotfiles: Install fish"
 echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_9.0/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
 curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_9.0/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
 sudo apt-get update
 sudo apt-get install fish -y
 
 # Install starship binary binary
+echo "🚀 Dotfiles: Install starship binary"
 bash -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes --verbose
 
 # Add starship to bash
+echo "🚀 Dotfiles: Add starship to bash"
 echo "eval \"$(starship init bash)\"" >> ~/.bashrc
 
 # Change default shell to fish
-echo /usr/bin/fish | sudo tee -a /etc/shells
+echo "🚀 Dotfiles: Change default shell to fish"
+# echo /usr/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/bin/fish
 
 # Add starship to fish
+echo "🚀 Dotfiles: Add starship to fish"
 starship init fish >> ~/.config/fish/config.fish
 
 # Remove script

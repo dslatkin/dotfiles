06307e6ec83ca51a99e00acb1bf896765b6fe81d
Daniel Slatkin <danielslatkin@gmail.com>
2022-09-12 09:25:01 -0700

    improve code readability

--- .chezmoiscripts/run_onchange_install_packages.sh
+++ .chezmoiscripts/run_onchange_install_packages.sh
@@ -106,15 +106,18 @@ fi
 if ! command -v op &> /dev/null
 then
     curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-    sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+        sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | \
-    sudo tee /etc/apt/sources.list.d/1password.list > /dev/null
+        sudo tee /etc/apt/sources.list.d/1password.list > /dev/null
+
     sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
     curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
-    sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol > /dev/null
+        sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol > /dev/null
+
     sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
     curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-    sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+        sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+
     sudo apt-get -qq update
     sudo apt-get -q -y install --no-install-recommends 1password-cli
     op --version

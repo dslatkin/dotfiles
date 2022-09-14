0885d1791b0b5a216320f7d7d6d22db3001fa4c0
Daniel Slatkin <danielslatkin@gmail.com>
2022-09-14 12:54:56 -0700

    add comment

--- .chezmoiscripts/run_onchange_gnome_config.sh
+++ .chezmoiscripts/run_onchange_gnome_config.sh
@@ -4,6 +4,8 @@
 #
 # All config executed in this file should be idempotent since this file re-runs
 # on `chezmoi apply` any time the hash of its contents has changed.
+#
+# More on gnome settings here: https://askubuntu.com/a/984214/1624642 
 
 # Add maximize button
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"

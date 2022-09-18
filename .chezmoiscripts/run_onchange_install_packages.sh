ce6620166ab365b1710af44ccfe462cf84d67706
Daniel Slatkin <danielslatkin@gmail.com>
2022-09-17 17:10:38 -0700

    improved code consistency

--- .chezmoiscripts/run_onchange_install_packages.sh
+++ .chezmoiscripts/run_onchange_install_packages.sh
@@ -16,17 +16,17 @@ sudo apt-get -qq upgrade
 # Install VLC (Debian; for Mac, use brew cask)
 if ! command -v vlc &> /dev/null
 then
-    echo dotfiles: Installing VLC Player
+    echo "dotfiles: Installing VLC Player"
     sudo apt-get -q -y install --no-install-recommends vlc
     vlc --version
 else
-    echo dotfiles: VLC is already installed
+    echo "dotfiles: VLC is already installed"
 fi
 
 # Install VS Code (Debian; for Mac, use brew cask)
 if ! command -v code &> /dev/null
 then
-    echo dotfiles: Installing VS Code
+    echo "dotfiles: Installing VS Code"
     sudo apt-get -q -y install --no-install-recommends wget gpg
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
@@ -36,7 +36,7 @@ then
     sudo apt-get -q -y install --no-install-recommends apt-transport-https code
     code --version
 else
-    echo dotfiles: VS Code is already installed
+    echo "dotfiles: VS Code is already installed"
 fi
 
 # Install FiraCode font variant (Pop_OS!)
@@ -46,31 +46,31 @@ font_directory="$HOME/.local/share/fonts/"
 font_target="$font_directory/$font_filename"
 if ! [[ -f $font_target ]]
 then
-    echo 'dotfiles: Installing Fira Code from Nerd Fonts'
+    echo "dotfiles: Installing Fira Code from Nerd Fonts"
     mkdir -p "$font_directory"
     curl --fail --silent --show-error --location $font_source > "$font_target"
     echo "⚠️ You may need to configure VS Code's font family to use Fira Code:"
     fc-list | grep "$font_target"
 else
-    echo dotfiles: Fira Code is already installed
+    echo "dotfiles: Fira Code is already installed"
 fi
 
 # Install Starship (Debian, but could also work on Mac - not sure if `/dev/null` exists)
 if ! command -v starship &> /dev/null
 then
-    echo dotfiles: Installing Starship
+    echo "dotfiles: Installing Starship"
     brew install --quiet starship
     eval "$(starship init bash)"
     eval "$(starship completions bash)"
     starship --version
 else
-    echo dotfiles: Starship is already installed
+    echo "dotfiles: Starship is already installed"
 fi
 
 # Install Docker (Ubuntu)
 if ! command -v docker &> /dev/null
 then
-    echo dotfiles: Installing Docker engine
+    echo "dotfiles: Installing Docker engine"
     sudo apt-get -q -y install --no-install-recommends ca-certificates curl gnupg lsb-release
     sudo mkdir -p /etc/apt/keyrings
     curl --fail --silent --show-error --location=https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -83,7 +83,7 @@ then
     sudo usermod -aG docker "$USER"
     newgrp docker
 else
-    echo dotfiles: Docker is already installed
+    echo "dotfiles: Docker is already installed"
 fi
 
 # Install Brave for a Chromium browser experience (Debian)
@@ -92,6 +92,7 @@ fi
 # https://www.reddit.com/r/pop_os/comments/f3s4c4/comment/fhp70xs
 if ! command -v brave-browser &> /dev/null
 then
+    echo "dotfiles: Installing Brave"
     sudo apt-get -q -y install --no-install-recommends apt-transport-https curl
     sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null
@@ -99,13 +100,13 @@ then
     sudo apt-get -q -y install --no-install-recommends brave-browser
     brave-browser --version
 else
-    echo dotfiles: Brave is already installed
+    echo "dotfiles: Brave is already installed"
 fi
 
 # Install 1Password desktop app and CLI (Debian)
 if ! command -v 1password &> /dev/null || ! command -v op &> /dev/null
 then
-    echo dotfiles: Installing 1Password desktop app and CLI
+    echo "dotfiles: Installing 1Password desktop app and CLI"
 
     curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
         sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
@@ -128,21 +129,21 @@ fi
 # Install nushell (brew)
 if ! command -v nu &> /dev/null
 then
-    echo dotfiles: Installing Nushell
+    echo "dotfiles: Installing Nushell"
     brew install --quiet nushell
     nu --version
 else
-    echo dotfiles: Nushell is already installed
+    echo "dotfiles: Nushell is already installed"
 fi
 
 # Install byobu (Debian)
 if ! command -v byobu &> /dev/null
 then
-    echo dotfiles: Installing Byobu
+    echo "dotfiles: Installing Byobu"
     sudo apt-get -q -y install --no-install-recommends byobu
     byobu --version
 else
-    echo dotfiles: Byobu is already installed
+    echo "dotfiles: Byobu is already installed"
 fi
 
 # Install stable diffusion deps (Debian)
@@ -158,20 +159,19 @@ fi
 # python scripts/txt2img.py --prompt "cowboy riding a unicorn over a rainbow" --plms --ckpt sd-v1-4.ckpt --skip_grid --n_samples 1
 if ! command -v python3 &> /dev/null
 then
-    echo dotfiles: Installing Python
+    echo "dotfiles: Installing Python"
     sudo apt-get -q -y install --no-install-recommends python3
     python3 --version
 else
-    echo dotfiles: Python is already installed
+    echo "dotfiles: Python is already installed"
 fi
 if ! command -v conda &> /dev/null
 then
-    echo dotfiles: Installing conda
+    echo "dotfiles: Installing conda"
 
     curl -sS https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc | \
         sudo gpg --dearmor --output /usr/share/keyrings/conda-archive-keyring.gpg
     gpg --keyring /usr/share/keyrings/conda-archive-keyring.gpg --no-default-keyring --fingerprint 34161F5BF5EB1D4BFBBB8F0A8AEB4F8B29D82806
-
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/conda-archive-keyring.gpg] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" | \
         sudo tee /etc/apt/sources.list.d/conda.list > /dev/null
 
@@ -179,7 +179,6 @@ then
     sudo apt-get -q -y install --no-install-recommends conda
 
     source /opt/conda/etc/profile.d/conda.sh
-
     conda init bash
     
     # todo: needs testing
@@ -187,5 +186,5 @@ then
 
     conda --version
 else
-    echo dotfiles: Conda is already installed
+    echo "dotfiles: Conda is already installed"
 fi

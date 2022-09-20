5d928758d46756b4bcbf6d3fd64c384f50c9b03e
Daniel Slatkin <danielslatkin@gmail.com>
2022-09-20 06:37:16 -0700

    add zsh compatibility for Mac

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "dotfiles: $0"
 set -euo pipefail
 
-# Install brew (Linux)
+# Install brew (Mac and Linux)
 if ! command -v brew &> /dev/null
 then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -14,9 +14,16 @@ then
     brew install chezmoi
 fi
 
-# Init
+# Init bash
 echo 'if [ -f ~/.dotfiles/bashrc.sh ]; then
 	source ~/.dotfiles/bashrc.sh
 fi' >> ~/.bashrc
+
+# Init zsh
+echo 'if [ -f ~/.dotfiles/zshrc.sh ]; then
+	source ~/.dotfiles/zshrc.sh
+fi' >> ~/.zshrc
+
+# Init chezmoi
 chezmoi init dslatkin/dotfiles --apply
-echo "dotfiles: Run \"exec bash\" to get your shell changes"
+echo "dotfiles: Run \"exec bash\" or \"exec zsh\" to get your shell changes"

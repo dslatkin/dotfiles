2ab3f82e000c83a355dce854ac060d1f9ad60541
Daniel Slatkin <danielslatkin@gmail.com>
2022-09-25 22:45:21 -0700

    refactor shell init scripts

--- install.sh
+++ install.sh
@@ -14,18 +14,6 @@ then
     brew install chezmoi
 fi
 
-# Init bash
-# todo: Have this create the runtime config if needed
-echo 'if [ -f ~/.dotfiles/bashrc.sh ]; then
-	source ~/.dotfiles/bashrc.sh
-fi' >> ~/.bashrc
-
-# Init zsh
-# todo: Have this create the runtime config if needed
-echo 'if [ -f ~/.dotfiles/zshrc.sh ]; then
-	source ~/.dotfiles/zshrc.sh
-fi' >> ~/.zshrc
-
 # Init chezmoi
 chezmoi init dslatkin/dotfiles --apply
 echo "dotfiles: Run \"exec bash\" or \"exec zsh\" to get your shell changes"

e59ffc0113e7e29b5ddf7f17e19f2a6eebf81cd8
Daniel Slatkin <danielslatkin@gmail.com>
2022-10-25 23:12:37 -0700

    refactor fzf runtime configs

--- dot_dotfiles/includes/fzf.bash
+++ dot_dotfiles/includes/fzf.bash
@@ -1,15 +1,12 @@
-{{ if or .env.mac .env.linux -}}
+# Initially generated from $(brew --prefix)/opt/fzf/install
+
 # Setup fzf
-# ---------
 if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
 # Auto-completion
-# ---------------
 [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
-# ------------
 source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
-{{ end -}}

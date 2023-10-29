daef4aac153713f01d9ac17c2bf62c259f0dcfa5
Daniel Slatkin <danielslatkin@gmail.com>
2023-10-29 19:48:59 +0000

    add debug output

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+touch ~/install-worked
+
 # Install brew (Mac and Linux)
 if ! command -v brew > /dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"

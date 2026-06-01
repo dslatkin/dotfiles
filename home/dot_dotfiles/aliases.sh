ebd0baa0dbea29afeeb211699fb521a3e380e073
Daniel Slatkin <danielslatkin@gmail.com>
2026-06-01 10:50:00 -0700

    simplify shell runtime config structure

---
+++ home/dot_dotfiles/aliases.sh
@@ -0,0 +1,10 @@
+# shellcheck shell=bash
+
+alias cz=chezmoi
+
+# alias l='ls -CF'
+# alias la='ls -A'
+# alias ll='ls -alF'
+# alias ls='ls --color=auto'
+# alias cat='bat --theme=base16'
+# alias grep='grep --color=auto'
---
+++ home/dot_dotfiles/env.sh
@@ -0,0 +1,4 @@
+# shellcheck shell=bash
+
+export HOMEBREW_NO_ENV_HINTS=1
+export HOMEBREW_NO_ANALYTICS=1

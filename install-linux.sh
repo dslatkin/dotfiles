78e85a5326524315f46c826d327d146876ce57a0
Daniel Slatkin <danielslatkin@gmail.com>
2026-09-10 14:45:51 -0700

    add container dotfiles WIP

--- install-linux.sh
+++ install-linux.sh
@@ -4,12 +4,20 @@ set -euo pipefail
 
 if ! command -v brew > /dev/null; then
     echo "💬 Installing brew"
-    /bin/bash -c "$(curl \
+    brew_env=()
+    if [[ "${REMOTE_CONTAINERS:-}" == "true" || "${CODESPACES:-}" == "true" ]]; then
+        brew_env+=(NONINTERACTIVE=1)
+    fi
+    env "${brew_env[@]}" /bin/bash -c "$(curl \
         --fail \
         --silent \
         --show-error \
         --location https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
     )"
+
+    # This dir is standard via Brew install script
+    brew_dir=/home/linuxbrew/.linuxbrew
+    eval "$($brew_dir/bin/brew shellenv bash)"
 fi
 
 if ! command -v chezmoi > /dev/null; then
@@ -17,13 +25,22 @@ if ! command -v chezmoi > /dev/null; then
     brew install chezmoi
 fi
 
-echo "💬 Initializing chezmoi"
-chezmoi init dslatkin/dotfiles --apply
+# echo "💬 Initializing chezmoi"
+# chezmoi_options=(--apply)
+# if [[ "${CODESPACES:-}" == "true" ]]; then
+#     # Source dir location cannot change and is documented here
+#     # https://docs.github.com/en/codespaces/troubleshooting/troubleshooting-personalization-for-codespaces#troubleshooting-dotfiles
+#     codespaces_dotfiles_dir=/workspaces/.codespaces/.persistedshare/dotfiles
+#     chezmoi_options+=(--source "$codespaces_dotfiles_dir")
+# fi
 
-echo "💬 Configuring .bashrc"
-cat <<EOF >> "$HOME/.bashrc"
-# Added from dotfiles install script
-source "\$HOME/.dotfiles/bashrc.sh"
-EOF
+# chezmoi init "${chezmoi_options[@]}" dslatkin/dotfiles
+# # chezmoi init dslatkin/dotfiles --apply
 
-echo "✅ Dotfiles installed, run \"exec bash\" to get shell changes"
+# # echo "💬 Configuring .bashrc"
+# # cat <<EOF >> "$HOME/.bashrc"
+# # # Added from dotfiles install script
+# # source "\$HOME/.dotfiles/bashrc.sh"
+# # EOF
+
+# # echo "✅ Dotfiles installed, run \"exec bash\" to get shell changes"
